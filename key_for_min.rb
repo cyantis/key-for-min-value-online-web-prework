b194277e41b46724806d5e1c1f38114e5000d3de
Cody Yantis <cyantis@gmail.com>
2019-01-29 03:58:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 def key_for_min_value(name_hash)
   low_k = nil
   low_v = nil
-  name_hash.each |k, v|
+  name_hash.each do |k, v|
     if low_v == nil || v < low_v
       low_k = k
       low_v = v
